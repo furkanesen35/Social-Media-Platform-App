f554a2d08061d5909eeab782eec03671dfb1dfa7
Furkan Esen <furkanesen35@gmail.com>
2024-01-24 11:22:29 +0100

    last day

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -13,8 +13,10 @@ import {
 import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import { z } from "zod"
+import Loader from "@/components/shared/Loader"
 
 const SignupForm = () => {
+ const isLoading = true
  // 1. Define your form.
  const form = useForm<z.infer<typeof SignupValidation>>({
   resolver: zodResolver(SignupValidation),
@@ -33,25 +35,78 @@ const SignupForm = () => {
  }
  return (
   <Form {...form}>
-   <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-    <FormField
-     control={form.control}
-     name="username"
-     render={({ field }) => (
-      <FormItem>
-       <FormLabel>Username</FormLabel>
-       <FormControl>
-        <Input placeholder="shadcn" {...field} />
-       </FormControl>
-       <FormDescription>
-        This is your public display name.
-       </FormDescription>
-       <FormMessage />
-      </FormItem>
-     )}
-    />
-    <Button type="submit">Submit</Button>
-   </form>
+   <div className="sm:w-420 flex-center flex-col">
+    <img src="/assets/images/logo.svg" alt="" />
+    <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create new Account</h2>
+    <p className="text-light-3 small-medium md:base-regular">To use your Snapgram enter your  details</p>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+     <FormField
+      control={form.control}
+      name="name"
+      render={({ field }) => (
+       <FormItem>
+        <FormLabel>Name</FormLabel>
+        <FormControl>
+         <Input type="text" className="shad-input" {...field} />
+        </FormControl>
+        <FormDescription>
+        </FormDescription>
+        <FormMessage />
+       </FormItem>
+      )}
+     />
+     <FormField
+      control={form.control}
+      name="username"
+      render={({ field }) => (
+       <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+         <Input type="text" className="shad-input" {...field} />
+        </FormControl>
+        <FormDescription>
+        </FormDescription>
+        <FormMessage />
+       </FormItem>
+      )}
+     />
+     <FormField
+      control={form.control}
+      name="email"
+      render={({ field }) => (
+       <FormItem>
+        <FormLabel>Email</FormLabel>
+        <FormControl>
+         <Input type="email" className="shad-input" {...field} />
+        </FormControl>
+        <FormDescription>
+        </FormDescription>
+        <FormMessage />
+       </FormItem>
+      )}
+     />
+     <FormField
+      control={form.control}
+      name="password"
+      render={({ field }) => (
+       <FormItem>
+        <FormLabel>Password</FormLabel>
+        <FormControl>
+         <Input type="password" className="shad-input" {...field} />
+        </FormControl>
+        <FormDescription>
+        </FormDescription>
+        <FormMessage />
+       </FormItem>
+      )}
+     />
+     <Button type="submit" className="shad-button_primary">
+      {isLoading ? (
+       <div className="flex-center gap-2"><Loader/>Loading...</div>
+      ) : "Signup"}
+     </Button>
+    </form>
+   </div>
   </Form>
  )
 }
